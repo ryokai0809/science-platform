55d66e6cb87f4eb24b174e48963d5677a89543e8
Ryokai Saito <ryokaisaito@Ryokais-MacBook-Air.local>
2025-06-15 02:58:09 +0900

    fix: move eslint config to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
-import type { NextConfig } from "next";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
