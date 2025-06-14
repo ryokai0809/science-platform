64a125b3ef3eafca5fe3992e1e5ff0b8590df466
Ryokai Saito <ryokaisaito@Ryokais-MacBook-Air.local>
2025-06-15 05:54:37 +0900

    fix: add Video type for videos

--- src/types/subject.ts
+++ src/types/subject.ts
@@ -1,9 +1,9 @@
-// src/types/subject.ts
+// Subject 타입 정의를 이렇게 수정
 export type Subject = {
   id: number;
   name: string;
   subject_id?: number;
   subjects?: {
     name: string;
-  };
+  }[]; // <- 배열로 변경
 };
