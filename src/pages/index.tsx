765bdfc98d647fc702b5b345446336740ead7c8c
Ryokai Saito <ryokaisaito@Ryokais-MacBook-Air.local>
2025-06-15 22:20:10 +0900

    fix: add Video type for videos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -156,7 +156,7 @@ export default function Home() {
           </div>
         ))}
         {!paid && (
-          <Button className="bg-[#EA6137] text-white" onClick={handlePayment}>
+          <Button className="text-primary" onClick={handlePayment}>
             라이센스 구매 ($70 / 1년)
           </Button>
         )}
